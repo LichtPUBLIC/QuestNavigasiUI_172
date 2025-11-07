05876d3ecb77f34e8740acf417c1bb08de3bcf91
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-07 14:29:31 +0700

    Implementasi pilihan 'Jenis Kelamin'

--- app/src/main/java/com/example/navigasi/view/Formulir.kt
+++ app/src/main/java/com/example/navigasi/view/Formulir.kt
@@ -58,6 +58,18 @@ fun FormIsian(
             HorizontalDivider(modifier = Modifier
                 .padding(20.dp)
                 .width(250.dp), thickness = Thickness,color = Color.Red)
+            Row {
+                jenisK.forEach {
+                        item->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
 
 
         }
