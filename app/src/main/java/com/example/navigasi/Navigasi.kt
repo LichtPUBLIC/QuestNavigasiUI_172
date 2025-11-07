df6921f26063a7b8b83e08a46424b396700e9b62
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-07 14:43:35 +0700

    Inisialisasi NavHost dan rute enum

---
+++ app/src/main/java/com/example/navigasi/Navigasi.kt
@@ -0,0 +1,34 @@
+package com.example.navigasi
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.navigasi.view.FormIsian
+import com.example.navigasi.view.TampilData
+
+enum class Navigasi {
+    Formulirku,
+    Detail
+}
+
+@Composable
+fun DataApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    Scaffold { isiRuang ->
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)){
+
+        }
+    }
+}
+
